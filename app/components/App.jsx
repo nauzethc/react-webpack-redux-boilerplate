18a2fa1cb38a5e49c0f81c130eea32a86bf2f2e3
Nauzet Hernandez <nauzet@aiakos.local>
2015-09-26 17:34:40 +0100

    Now App is a stateless React component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import actions from '../actions';
 import '../styles/App.css';
 
 
 class App extends React.Component {
 
+  addItem() {
+    this.props.dispatch(
+      actions.addItem({
+        id: this.props.counter,
+        text: 'New Item ' + this.props.counter
+      })
+    );
+    this.props.dispatch(actions.increment());
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Hello, React!</h1>
+        <h1>Hello, React! <a href="#" onClick={this.addItem.bind(this)}>+</a></h1>
+        <ul>
+          { this.props.items.map(item => {
+            return <li>{ item.text }</li>;
+          }) }
+        </ul>
       </div>
     );
   }
