707b79bdfbf5cc33156cddb32a57f65edfd9161e
Nauzet Hernandez <nauzet@aiakos.local>
2015-09-27 22:53:39 +0100

    Added reducer for single item and REMOVE_ITEM action handlers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,21 +4,59 @@ import { combineReducers } from 'redux';
 
 // Add reducers or import them from other modules
 
-function counter(state = 1, action) {
-  switch(action.type) {
-    case (types.INCREMENT):
-      return state + 1;
+function item(state = {}, action) {
+  switch (action.type) {
+
+    case types.ADD_ITEM:
+    case types.EDIT_ITEM:
+      return Object.assign({}, {...state}, { text: action.text });
 
     default:
       return state;
   }
 }
 
-function items(state = [], action) {
+function itemsById(state = {}, action) {
+  let newState;
+  switch(action.type) {
+
+    case types.ADD_ITEM:
+      newState = {};
+      newState[action.id] = item({ id: action.id }, action);
+      return Object.assign({}, {...state, ...newState});
+
+    case types.REMOVE_ITEM:
+      newState = {};
+      Object.keys(state)
+        .filter(id => id != action.id)
+        .forEach(id => newState[id] = Object.assign({}, state[id]));
+      return newState;
+
+    case types.EDIT_ITEM:
+      newState = {};
+      newState[action.id] = item(state[action.id], action);
+      return Object.assign({}, {...state}, {...newState});
+
+    default:
+      return state;
+  }
+}
+
+function data(state = { counter: 0, itemsById: {} }, action) {
 
   switch(action.type) {
-    case (types.ADD_ITEM):
-      return [...state, { id: action.id, text: action.text }];
+    case types.ADD_ITEM:
+      return Object.assign({}, {
+        counter: state.counter + 1,
+        itemsById: itemsById(state.itemsById, {...action, id: state.counter + 1})}
+      );
+
+    case types.REMOVE_ITEM:
+    case types.EDIT_ITEM:
+      return Object.assign({}, {
+        counter: state.counter,
+        itemsById: itemsById(state.itemsById, action)
+      });
 
     default:
       return state;
@@ -29,4 +67,4 @@ function items(state = [], action) {
 
 // Export reducers as global reducer
 
-export default combineReducers({ items, counter });
+export default combineReducers({ data });
