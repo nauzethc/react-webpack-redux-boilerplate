e792a1ee32e4f33659fe97889f2c06bbd445241a
Nauzet Hernandez <nauzet@aiakos.local>
2015-09-27 22:52:41 +0100

    REMOVE_ITEM added

--- app/actions/index.js
+++ app/actions/index.js
@@ -3,24 +3,33 @@ import types from './types';
 
 // Add your actions
 
-export function increment() {
-  return {
-    type: types.INCREMENT
-  };
-};
-
-export function addItem({id, text}) {
+export function addItem(text) {
   return {
     type: types.ADD_ITEM,
-    id,
     text
   }
 };
+
+export function removeItem(id) {
+  return {
+    type: types.REMOVE_ITEM,
+    id
+  }
+}
+
+export function editItem(id, text) {
+  return {
+    type: types.EDIT_ITEM,
+    id,
+    text
+  }
+}
 
 
 // Export all by default
 
 export default {
   addItem,
-  increment
+  removeItem,
+  editItem
 };
