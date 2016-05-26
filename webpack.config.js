ad39dd5ba2ed042718f74f483d2ade43c460de77
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2016-05-26 13:16:20 +0100

    webpack config rewritten to use new modules behaviours

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,19 @@
-var pkg = require('./package.json');
-var path = require('path');
-var Webpack = require('webpack');
-var merge = require('webpack-merge');
-var HtmlPlugin = require('html-webpack-plugin');
-var CleanPlugin = require('clean-webpack-plugin');
+var path              = require('path');
+var pkg               = require('./package.json');
+var webpack           = require('webpack');
+var HtmlPlugin        = require('html-webpack-plugin');
+var CleanPlugin       = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-// Defaults
+var TARGET      = process.env.npm_lifecycle_event;
+var ROOT        = path.resolve(__dirname);
 
-var TARGET = process.env.npm_lifecycle_event;
-var ROOT = path.resolve(__dirname);
-
-var common = {
-
-  entry: [
-    path.resolve(ROOT, 'app')
-  ],
-
-  resolve: ['', '.js', '.jsx'],
-
-  output: {
-    path: path.resolve(ROOT, 'build'),
-    filename: 'bundle.js'
-  },
-
-  module: {
-    preLoaders: [
-      { test: /\.css$/, loaders: ['csslint'], include: path.resolve(ROOT, 'app') },
-      { test: /\.jsx?$/, loaders: ['eslint'], include: path.resolve(ROOT, 'app') }
-    ]
-  },
-
-  plugins: [
-    new HtmlPlugin({
-      title: 'React App',
-      inject: 'body',
-      template: path.resolve(ROOT, 'app/templates/index.html')
-    })
-  ]
-
-};
 
 // Development
 
-if (TARGET === 'start' || TARGET === 'debug' || !TARGET) {
+if (TARGET === 'start' || !TARGET) {
 
-  module.exports = merge(common, {
+  module.exports = {
 
     devtool: 'eval-source-map',
 
@@ -56,66 +24,136 @@ if (TARGET === 'start' || TARGET === 'debug' || !TARGET) {
       progress: true
     },
 
-    module: {
-      loaders: [
-        { test: /\.css$/, loaders: ['style', 'css'], include: path.resolve(ROOT, 'app') },
-        { test: /\.jsx?$/, loaders: ['babel'], include: path.resolve(ROOT, 'app') }
-      ]
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.css']
     },
 
-    plugins: [
-      new Webpack.HotModuleReplacementPlugin(),
-      new Webpack.DefinePlugin({ 'DEBUG': TARGET === 'debug' })
-    ]
-
-  });
-
-// Production
-
-} else if (TARGET === 'build') {
-
-  module.exports = merge(common, {
-
     entry: [
-      {
-        app: path.resolve(ROOT, 'app'),
-        vendor: Object.keys(pkg.dependencies)
-      }
+      'webpack-hot-middleware/client',
+      path.resolve(ROOT, 'app'),
     ],
 
     output: {
       path: path.resolve(ROOT, 'build'),
-      filename: '[name].js?[chunkhash]'
+      publicPath: '/',
+      filename: 'bundle.js'
     },
 
-    devtool: 'source-map',
-
     module: {
+      preLoaders: [
+        {
+          test: /\.css$/,
+          loader: 'csslint',
+          include: path.resolve(ROOT, 'app')
+        }, {
+          test: /\.jsx?$/,
+          loader: 'eslint',
+          include: path.resolve(ROOT, 'app')
+        }
+      ],
       loaders: [
-        { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css'), include: path.resolve(ROOT, 'app') },
-        { test: /\.jsx?$/, loaders: ['babel'], include: path.resolve(ROOT, 'app') }
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: path.resolve(ROOT, 'app')
+        }, {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          include: path.resolve(ROOT, 'app'),
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'react-hmre', 'es2015'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
       ]
     },
 
     plugins: [
+      new HtmlPlugin({
+        title: 'React App',
+        inject: 'body',
+        template: path.resolve(ROOT, 'app/templates/index.html')
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({ 'DEBUG': true })
+    ]
+  };
+
+
+// Production
+
+} else {
+
+  module.exports = {
+
+    devtool: 'cheap-module-eval-source-map',
+
+    entry: {
+      app: path.resolve(ROOT, 'app'),
+      vendor: Object.keys(pkg.dependencies)
+    },
+
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.css']
+    },
+
+    output: {
+      path: path.resolve(ROOT, 'dist'),
+      filename: '[name].js?[chunkhash]'
+    },
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.css$/,
+          loader: 'csslint',
+          include: path.resolve(ROOT, 'app')
+        }, {
+          test: /\.jsx?$/,
+          loader: 'eslint',
+          include: path.resolve(ROOT, 'app')
+        }
+      ],
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+          include: path.resolve(ROOT, 'app')
+        }, {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          include: path.resolve(ROOT, 'app'),
+          query: {
+            presets: ['react', 'es2015'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlPlugin({
+        title: 'React App',
+        inject: 'body',
+        template: path.resolve(ROOT, 'app/templates/index.html')
+      }),
       new CleanPlugin(['build']),
       new ExtractTextPlugin('styles.css?[chunkhash]'),
-      new Webpack.optimize.CommonsChunkPlugin(
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new webpack.optimize.CommonsChunkPlugin(
         'vendor', '[name].js?[chunkhash]'
       ),
-      new Webpack.DefinePlugin({
-        'process.env': { 'NODE_ENV': JSON.stringify('production') }
+      new webpack.DefinePlugin({
+        'process.env'  : { 'NODE_ENV': JSON.stringify('production') }
       }),
-      new Webpack.optimize.UglifyJsPlugin({
+      new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
         }
       })
     ]
-  })
+  };
 
-} else {
-
-  module.exports = common;
-
-}
+};
