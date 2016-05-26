7f1cb691d444e515ffa574bfdbaa5e30f98c6a09
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2016-05-26 13:19:39 +0100

    Connector rewritten using functions. Actions are imported from 'ducks' files

--- app/connectors/items.js
+++ app/connectors/items.js
@@ -1,16 +1,37 @@
 import { connect } from 'react-redux';
+import {
+  createItem,
+  removeItem,
+  updateItem,
+  fetchItem,
+  toggleItem,
+  setFilter,
+} from '../ducks/items.duck';
 
 
-const ItemsConnector = (Component) => {
-  return connect((state) => {
-    return {
-      isFetching: state.items.isFetching,
-      items: Object.keys(state.items.data.itemsById)
-        .map(id => state.items.data.itemsById[id])
-    }
-  })(Component);
-};
+export default (Component) => connect(
 
-export default {
-  ItemsConnector,
-};
+  (state, props) => ({
+    isFetching: state.items.fetching,
+    filter: state.items.filter,
+    items: Object.keys(state.items.itemsById)
+      .map(id => state.items.itemsById[id])
+      .filter(item => {
+        if ((state.items.filter === 'Completed'   && !item.done) ||
+            (state.items.filter === 'Uncompleted' && item.done)) {
+            return false;
+        }
+        return true;
+      })
+  }),
+
+  (dispatch, props) => ({
+    onCreate: (text)     => dispatch(createItem(text)),
+    onRemove: (id)       => dispatch(removeItem(id)),
+    onUpdate: (id, text) => dispatch(updateItem(id, text)),
+    onToggle: (id)       => dispatch(toggleItem(id)),
+    onFetch:  (text)     => dispatch(fetchItem(text)),
+    onFilter: (filter)   => dispatch(setFilter(filter)),
+  })
+
+)(Component);
