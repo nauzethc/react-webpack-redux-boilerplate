128782f5c219e1ba5008c902d8e90f374dfe0eb1
Nauzet Hernandez <nauzethc@gmail.com>
2016-06-06 10:49:03 +0100

    Store middleware migrated to redux-saga

--- app/store/configure.dev.js
+++ app/store/configure.dev.js
@@ -1,26 +1,31 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import { persistState } from 'redux-devtools';
 import DevTools from '../containers/DevTools';
 import reducers from '../reducers';
 
+import itemsSaga from '../sagas/items.saga';
+
 
 export default function configure(initialState = {}) {
+
+  const sagaMiddleware = createSagaMiddleware();
 
   // Apply middlewares and create store
   const store = createStore(
     reducers,
     initialState,
     compose(
-      applyMiddleware(
-        thunkMiddleware
-      ),
+      applyMiddleware(sagaMiddleware),
       DevTools.instrument(),
       persistState(window.location.href.match(
         /[?&]debug_session=([^&]+)\b/
       ))
     )
   );
+
+  // Run sagas
+  sagaMiddleware.run(itemsSaga);
 
   // Re-add reducer on change if 'hot' module is present
   if (module.hot) {
--- app/store/configure.prod.js
+++ app/store/configure.prod.js
@@ -1,20 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import reducers from '../reducers';
+
+import itemsSaga from '../sagas/items.saga';
 
 
 export default function configure(initialState = {}) {
+
+  const sagaMiddleware = createSagaMiddleware();
 
   // Apply middlewares and create store
   const store = createStore(
     reducers,
     initialState,
-    compose(
-      applyMiddleware(
-        thunkMiddleware
-      )
-    )
+    compose(applyMiddleware(sagaMiddleware))
   );
+
+  // Run sagas
+  sagaMiddleware.run(itemsSaga);
 
   return store;
 }
